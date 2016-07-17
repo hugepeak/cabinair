50f057c69f3bf2c838db630e8bb2711c1d717197
hugepeak <hugepeak@ec8187f3-1607-42df-ab40-7d9c81190c62>
2016-07-17 14:07:17 +0000

    Change to use boost::array instead of std::vector, which is faster and uses slightly less memory. Use typedef to define containers.

--- trunk/src/network.h
+++ trunk/src/network.h
@@ -19,15 +19,43 @@
 #include <vector>
 #include <list>
 #include <ctime>
+#include <boost/array.hpp>
 
 #define I_X_SIZE                200    // x grid number
 #define I_Y_SIZE                200    // y grid number
-#define I_DEFAULT_LAYER_NUMBER  10     // default layer number
+#define I_LAYER                 10     // default layer number
 #define I_INITIAL_TIME_LENGTH   100    // seconds
 #define D_X_GRID_LENGTH         10.    // x grid length in meters
 #define D_Y_GRID_LENGTH         10.    // y grid length in meters
 #define D_X_MIN                 0.     // min x in meters, should be unsigned
 #define D_Y_MIN                 0.     // min y in meters, should be unsigned
+
+typedef
+  std::list<
+    boost::array<
+      boost::array<
+        boost::array<int,I_Y_SIZE>,I_X_SIZE
+      >,I_LAYER
+    >
+  > network_t;
+typedef
+  std::list<
+    boost::array<
+      boost::array<
+        boost::array<int,I_Y_SIZE>,I_X_SIZE
+      >,I_LAYER
+    >
+  >::iterator network_it_t;
+typedef
+  boost::array<
+    boost::array<
+      boost::array<int,I_Y_SIZE>,I_X_SIZE
+    >,I_LAYER
+  > cube_t;
+
+/*//////////////////////////////////////////////////////////////////////////////
+* Class Network.
+*///////////////////////////////////////////////////////////////////////////////
 
 class Network {
 
@@ -35,7 +63,7 @@ private:
 
   size_t layer_number;
   time_t first_time;
-  std::list<std::vector<std::vector<std::vector<int> > > > network;
+  network_t network;
 
 public:
 
@@ -47,22 +75,15 @@ public:
   void setFirstTime( time_t _first_time ) { first_time = _first_time; }
   time_t getFirstTime() { return first_time; }
 
-  std::list<std::vector<std::vector<std::vector<int> > > > getNetwork() {
-    return network;
-  }
-
+  int add_empty_cube();
   size_t getSize() { return network.size(); }
-  int add_empty_cubic();
   void popFront() { network.pop_front(); }
 
-  std::list<std::vector<std::vector<std::vector<int> > > >::iterator
-    getNetworkBegin() { return network.begin(); }
-  std::list<std::vector<std::vector<std::vector<int> > > >::iterator
-    getNetworkEnd() { return network.end(); }
-  std::vector<std::vector<std::vector<int> > > 
-    getNetworkBack() { return network.back(); }
+  network_it_t getNetworkBegin() { return network.begin(); }
+  network_it_t getNetworkEnd() { return network.end(); }
+  cube_t getNetworkBack() { return network.back(); }
   int getElement( 
-    std::list<std::vector<std::vector<std::vector<int> > > >::iterator it,
+    network_it_t it,
     size_t layer, 
     size_t xgrid, 
     size_t ygrid
@@ -70,7 +91,7 @@ public:
     return (*it)[layer][xgrid][ygrid]; 
   }
   void setElement( 
-    std::list<std::vector<std::vector<std::vector<int> > > >::iterator it,
+    network_it_t it,
     size_t layer, 
     size_t xgrid, 
     size_t ygrid,
@@ -86,6 +107,7 @@ public:
   ) {
     network.back()[layer][xgrid][ygrid] = value;
   }
+
   void writeOutput() {};
 
 };
